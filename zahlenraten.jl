3212e6f7f26c5464c3a1632db8fbeef7de91993d
Mira253 <mira.schmidt@stud.uni-hannover.de>
2025-05-13 14:50:54 +0200

    Zahlenratespiel modifiziert

--- zahlenraten.jl
+++ zahlenraten.jl
@@ -1,21 +1,39 @@
 # zahlenraten.jl
 println("Willkommen beim Zahlenratespiel!")
 
-geheimzahl = rand(1:100)
-versuche = 0
-gefunden = false
+function spiel()
+    geheimzahl = rand(1:100)  # Zufallszahl zwischen 1 und 100
+    versuche = 0  # Startwert für Versuche
+    gefunden = false  # Flag, das angibt, ob die Zahl gefunden wurde
 
-while !gefunden
-    print("Gib eine Zahl zwischen 1 und 100 ein: ")
-    eingabe = parse(Int, readline())
-    versuche += 1
+    while !gefunden
+        print("Gib eine Zahl zwischen 1 und 100 ein: ")
+        flush(stdout)  # Sicherstellen, dass der Prompt korrekt angezeigt wird
+        eingabe = readline()  # Liest die Benutzereingabe als String ein
+        if isempty(eingabe)  # Falls nichts eingegeben wird, nochmal abfragen
+            println("Bitte eine gültige Zahl eingeben.")
+            continue  # Überspringt den Rest und fragt erneut
+        end
 
-    if eingabe < geheimzahl
-        println("Zu klein!")
-    elseif eingabe > geheimzahl
-        println("Zu groß!")
-    else
-        println("Richtig! Du hast $versuche Versuche gebraucht.")
-        gefunden = true
+        try
+            eingabe = parse(Int, eingabe)  # Versuche, die Eingabe in eine Zahl zu konvertieren
+        catch e
+            println("Ungültige Eingabe! Bitte eine Zahl zwischen 1 und 100 eingeben.")
+            continue  # Falls die Eingabe keine Zahl ist, wiederholen
+        end
+
+        versuche += 1  # Erhöhe die Versuche
+
+        if eingabe < geheimzahl
+            println("Zu klein!")
+        elseif eingabe > geheimzahl
+            println("Zu groß!")
+        else
+            println("Richtig! Du hast $versuche Versuche gebraucht.")
+            gefunden = true  # Zahl gefunden, Spiel beendet
+        end
     end
 end
+
+# Start des Spiels
+spiel()
